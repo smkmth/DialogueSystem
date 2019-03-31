ee4eb505f4d451798611c85f891ed91705cf0f82
SmkMth <dandavidnoon@gmail.com>
2019-03-31 18:08:30 +0100

    Initial commit

---
+++ Assets/Editor/Connection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class Connection
+{
+    public ConnectionPoint inPoint;
+    public ConnectionPoint outPoint;
+    public Action<Connection> OnClickRemoveConnection;
+
+    public Connection(ConnectionPoint _inPoint, ConnectionPoint _outPoint, Action<Connection> _OnClickRemoveConnection)
+    {
+        this.inPoint = _inPoint;
+        this.outPoint = _outPoint;
+        this.OnClickRemoveConnection = _OnClickRemoveConnection;
+    }
+    
+    public void Draw()
+    {
+        Handles.DrawBezier(
+            inPoint.rect.center,
+            outPoint.rect.center,
+            inPoint.rect.center + Vector2.left * 50.0f,
+            inPoint.rect.center - Vector2.left * 50.0f,
+            Color.white,
+            null,
+            2.0f
+            );
+        if (Handles.Button((inPoint.rect.center + outPoint.rect.center) * 0.5f, Quaternion.identity, 4, 8, Handles.RectangleHandleCap))
+        {
+            if (OnClickRemoveConnection != null)
+            {
+                OnClickRemoveConnection(this);
+            }
+
+        }
+        
+    }
+
+
+}
---
+++ Assets/Editor/ConnectionPoint.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum ConnectionPointType { In, Out}
+
+public class ConnectionPoint  {
+
+    public Rect rect;
+
+    public ConnectionPointType type;
+
+    public DialogueNode node;
+
+    public GUIStyle style;
+
+    public Action<ConnectionPoint> OnClickConnectionPoint;
+
+    public ConnectionPoint(DialogueNode _node, ConnectionPointType _type, GUIStyle _style, Action<ConnectionPoint> _OnClickConnectionPoint)
+    {
+        this.node = _node;
+        this.type = _type;
+        this.style = _style;
+        this.OnClickConnectionPoint = _OnClickConnectionPoint;
+        rect = new Rect(0.0f, 0.0f, 10.0f, 20.0f);
+    }
+
+    public void Draw()
+    {
+        rect.y = node.rect.y + (node.rect.height * 0.5f) - rect.height * 0.5f;
+
+        switch (type)
+        {
+            case ConnectionPointType.In:
+                rect.x = node.rect.x - rect.width + 8f;
+                break;
+
+            case ConnectionPointType.Out:
+                rect.x = node.rect.x + node.rect.width - 8f;
+                break;
+        }
+
+        if (GUI.Button(rect, "", style))
+        {
+            if (OnClickConnectionPoint != null)
+            {
+                OnClickConnectionPoint(this);
+            }
+        }
+    }
+}
